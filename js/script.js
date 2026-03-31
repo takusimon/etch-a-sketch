26171e81e993e0ef457d0b8b7da5bf8aa91e5f9a
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-30 19:02:31 -0500

    Etch-A-Sketch
    created a program that allows you to etch a Sketch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const container = document.querySelector(".container")
-for (let b=0; b< 16; b++){
-    for (let i=0;i<16;i++) {
-        const gridBox = document.createElement('div');
-        gridBox.setAttribute("style", "width:50px; height:50px; border:1px solid black;");
-        container.appendChild(gridBox);
+
+function gridCreation (boxcount=16){
+    const boxDimensions = 960/boxcount;
+    container.innerHTML = "";
+    for (let b=0; b< boxcount; b++){
+        for (let i=0;i<boxcount;i++) {
+            const gridBox = document.createElement('div');
+            gridBox.setAttribute("style", `width:${boxDimensions}px; height:${boxDimensions}px; border:1px solid black; boxSizing:border-box;`);
+            gridBox.addEventListener('mouseover', () => gridBox.classList.add('hovered'));
+            gridBox.addEventListener('mouseout',() => gridBox.classList.add('trail'));
+            container.appendChild(gridBox);
+        };
     };
 };
+gridCreation();
+
+const gridButton = document.querySelector('.grid-specs');
+gridButton.addEventListener('click', () =>{
+    let userInput = prompt('Enter a grid value e.g. 16 = 16 * 16 grid');
+
+    if (1 <= userInput && userInput <= 100){
+        gridCreation(userInput);
+    } else {
+        alert("This is not a valid value");
+    }
+})
